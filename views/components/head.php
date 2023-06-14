eac53901455fef9991321d2ea1363678c0e9f977
Allan DEHORS <allandhs@MacBook-Pro-de-Allan.local>
2023-06-14 09:22:37 +0200

    AjoutDuTitre

--- views/components/head.php
+++ views/components/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,4 +8,11 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../../node_modules/animate.css/animate.min.css">
 </head>
+
 <body>
+    <header>
+        <h1>TechPeripherals</h1>
+        <P>Le site n°1 pour la vente des
+            <br> péripheriques de GAMEEEEER
+        </P>
+    </header>
